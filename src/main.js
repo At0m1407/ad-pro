9a473aff89403c4673f42b23bcb3d4a978d4db58
At0m1407 <102040155+At0m1407@users.noreply.github.com>
2022-03-24 21:52:25 +0300

    br_13.1_13.2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +15,21 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created(){
+const firebaseConfig = {
+  apiKey: "AIzaSyBM9eIsWH-690AzrhP7KKxpym8dE0ZtTfo",
+  authDomain: "ad-pro-14816.firebaseapp.com",
+  projectId: "ad-pro-14816",
+  storageBucket: "ad-pro-14816.appspot.com",
+  messagingSenderId: "42083426722",
+  appId: "1:42083426722:web:20324462598e42a1b992fb",
+  measurementId: "G-JPB7L4Z34Y"
+};
+ // Initialize Firebase
+  fb.initializeApp(firebaseConfig);
+  fb.analytics();
+  const app = initializeApp(firebaseConfig);
+  getAnalytics(app);
+}
 }).$mount('#app')
